68fe2749bd8ef94b8c1f44bb35415db99cb20306
Jamorn <mitterkit@gmail.com>
2026-07-31 12:26:27 +0700

    chore: add ktlint plugin and format codebase
    - Add org.jlleitschuh.gradle.ktlint v14.2.0 (root + app)
    - Configure android=true (4-space indent), ignoreFailures=true
    - ktlintFormat applied

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.50" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "14.2.0" apply false
 }
+
